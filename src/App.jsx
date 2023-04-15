244e658278adf867873c2c59a057f9ff35e00877
Flávio Dutra <flaviomdutra@outlook.com>
2023-04-15 15:30:29 -0300

    feat: add features

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,61 @@ import styles from "./App.module.css";
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galera 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no  meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <>
       <Header />
-      
+
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Flávio Dutra"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit autem debitis unde beatae a architecto, ea iure, odit quod dolores nesciunt quia laborum quasi recusandae ut blanditiis cupiditate, voluptatem possimus."
-          />
-          <Post
-            author="Daiane Dutra"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit autem debitis unde beatae a architecto, ea iure, odit quod dolores nesciunt quia laborum quasi recusandae ut blanditiis cupiditate, voluptatem possimus."
-          />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import { ThumbsUp, Trash } from "phosphor-react";
-import styles from "./Comment.module.css";
+
 import { Avatar } from "./Avatar";
 
-export function Comment() {
+import styles from "./Comment.module.css";
+
+export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar src="https://github.com/flaviomdutra.png" hasBorder={false} />
@@ -17,16 +30,16 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Deleter comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={20} />
             </button>
           </header>
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
+          <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
